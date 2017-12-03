58e31531a57170009c7a3f2c3505a02f2d15af1f
andrejs82 <andrejs82@gmail.com>
2017-12-03 20:43:32 -0300

    add strategy for print text

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const LEAD_SPACE = '  ';
+const LEAD_SPACE = ' ';
 
 export const getFileExt = fpath => path.extname(fpath).replace('.', '');
 
@@ -17,35 +17,38 @@ const parseConfig = (configText, configType) => {
   return parsers[configType](configText);
 };
 
-const astToText = (model) => {
-  const objToJson = (val, depth) => JSON.stringify(val, null, LEAD_SPACE.length)
-    .replace(/^/gm, `${LEAD_SPACE.repeat(depth + 1)}`)
-    .replace(/([^\s]+:)/g, '  $1')
-    .replace(/[",]/g, '')
-    .trim();
+const objToJson = (val, depth) => JSON.stringify(val, null, 4)
+  .replace(/^/gm, `${LEAD_SPACE.repeat(depth * 4)}`)
+  .replace(/[",]/g, '')
+  .trim();
 
+const astToTextStrategy = {
+  rootDiff: (node, depth, body) => `{${body}\n}`,
+  complex: (node, depth, body) => `\n${LEAD_SPACE.repeat(depth * 4)}${node.key}: {${body}\n${LEAD_SPACE.repeat(depth * 4)}}`,
+  equal: (node, depth) => `\n${LEAD_SPACE.repeat(depth * 4)}${node.key}: ${node.val}`,
+  add: (node, depth) => `\n${LEAD_SPACE.repeat((depth * 4) - 2)}+ ${node.key}: ${objToJson(node.val, depth)}`,
+  remove: (node, depth) => `\n${LEAD_SPACE.repeat((depth * 4) - 2)}- ${node.key}: ${objToJson(node.val, depth)}`,
+  change: (node, depth) => `\n${LEAD_SPACE.repeat((depth * 4) - 2)}+ ${node.key}: ${objToJson(node.vala, depth)}\n${LEAD_SPACE.repeat((depth * 4) - 2)}- ${node.key}: ${objToJson(node.valb, depth)}`,
+};
+
+const astToText = (model, printStrategy) => {
   const iter = (node, depth = 0) => {
-    if (_.isArray(node)) {
-      const result = node.reduce((acc, childNode) => `${acc}${iter(childNode, depth + 1)}`, '');
-      return `{${result}\n${LEAD_SPACE.repeat(depth)}}`;
-    }
-    const { type, key } = node;
-    switch (type) {
-      case 'complex': return `\n${LEAD_SPACE.repeat(depth)}  ${key}: ${iter(node.body, depth + 1)}`;
-      case 'equal': return `\n${LEAD_SPACE.repeat(depth)}  ${key}: ${node.val}`;
-      case 'add': return `\n${LEAD_SPACE.repeat(depth)}+ ${key}: ${objToJson(node.val, depth)}`;
-      case 'remove': return `\n${LEAD_SPACE.repeat(depth)}- ${key}: ${objToJson(node.val, depth)}`;
-      case 'change': return `\n${LEAD_SPACE.repeat(depth)}+ ${key}: ${objToJson(node.vala, depth)}\n${LEAD_SPACE.repeat(depth)}- ${key}: ${objToJson(node.valb, depth)}`;
-      default: throw Error('Tree not valid!');
+    switch (node.type) {
+      case 'complex':
+      case 'rootDiff': {
+        const body = node.body.reduce((acc, childNode) => `${acc}${iter(childNode, depth + 1)}`, '');
+        return printStrategy[node.type](node, depth, body);
+      }
+      default: return printStrategy[node.type](node, depth);
     }
   };
   return iter(model);
 };
 
-const createDiffAst = (beforeObject, afterObject) => {
+const iterDiffAst = (beforeObject, afterObject) => {
   const iter = (beforeProperty, afterProperty, key) => {
     if (_.isObject(beforeProperty) && _.isObject(afterProperty)) {
-      return { key, type: 'complex', body: createDiffAst(beforeProperty, afterProperty) };
+      return { key, type: 'complex', body: iterDiffAst(beforeProperty, afterProperty) };
     } else if (beforeProperty === afterProperty) {
       return { key, type: 'equal', val: beforeProperty };
     } else if (typeof beforeProperty === 'undefined') {
@@ -67,10 +70,13 @@ const createDiffAst = (beforeObject, afterObject) => {
     [...acc, iter(beforeObject[key], afterObject[key], key)], []);
 };
 
+const createDiffAst = (beforeObject, afterObject) =>
+  ({ type: 'rootDiff', body: iterDiffAst(beforeObject, afterObject) });
+
 const gendiff = (firstConfigFilePath, secondConfigFilePath) => {
   const firstConfig = parseConfig(fs.readFileSync(firstConfigFilePath, 'utf8'), getFileExt(firstConfigFilePath));
   const secondConfig = parseConfig(fs.readFileSync(secondConfigFilePath, 'utf8'), getFileExt(secondConfigFilePath));
-  return astToText(createDiffAst(firstConfig, secondConfig));
+  return astToText(createDiffAst(firstConfig, secondConfig), astToTextStrategy);
 };
 
 export default gendiff;
